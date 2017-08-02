091e2d92fac1608f616998909425b451f10590f3
Coeusguo <coeusguosp@gmail.com>
2017-08-02 20:42:28 +0800

    initial commit

---
+++ dqn.py
@@ -0,0 +1,188 @@
+import sys
+sys.path.append("game/")
+import wrapped_flappy_bird as game
+import tensorflow as tf
+import numpy as np
+from network import Network
+from collections import deque
+import cv2
+import random
+import os
+
+IMAGE_SIZE = (80,80)
+BATCH_SIZE = 30
+GAMMA = 0.99
+EBSILON = 0.02
+UPDATE_STEP = 100
+FRAME_PER_ACTION = 4
+HOLD_ACTION = False
+REPLAY_MEMORY_SIZE = 50000
+EXPLORE_STEPS = 200000
+LEARNING_RATE = 1e-4
+CKPT_PATH = os.path.join(os.getcwd(), 'model', 'dqn')
+SAVE_STEP = 10000
+
+class DQN(Network):
+	def __init__(self, num_action, dataset = None, trainable = True): #data_shape = (height, width)
+		assert dataset == None
+		self.__data_shape = (IMAGE_SIZE[0], IMAGE_SIZE[1], 4)
+		self.__trainable = trainable
+		self.__replay_memory = deque()
+		self.__num_action = num_action
+
+		self.__train_input = tf.placeholder(tf.float32, shape = [None, self.__data_shape[0], self.__data_shape[1], self.__data_shape[2]], name = 'train_input')
+		self.__target_input = tf.placeholder(tf.float32, shape = [None, self.__data_shape[0], self.__data_shape[1], self.__data_shape[2]], name = 'target_input')
+
+		self.layers = {'train_input' : self.__train_input, 'target_input' : self.__target_input}
+
+		self.inputs = []
+		self.__session = tf.InteractiveSession()
+		self.__optimizer = None
+		
+
+		#list used to store two network layes
+		self.__train_variables = []
+		self.__target_variables = []
+
+		self.__current_state = None
+
+		self.__last_action = [0 for _ in range(num_action)]
+		self.__time_step = 0
+		self.__ebsilon = EBSILON
+		
+		self.setup()
+
+	def setup(self):
+		#train network
+		(self.feed('train_input')
+		.conv(8, 32, 4, 4, name = 'conv_train_1', appendList = self.__train_variables).max_pooling(name = 'max_pool_train_1')
+		.conv(4, 64, 2, 2, name = 'conv_train_2', appendList = self.__train_variables).max_pooling(name = 'max_pool_train_2')
+		.conv(3, 64, 1, 1, name = 'conv_train_3', appendList = self.__train_variables).max_pooling(name = 'max_pool_train_3')
+		.fc(512, name = 'fc_train_1', appendList = self.__train_variables)
+		.fc(self.__num_action, name = 'QValues', relu = False, appendList = self.__train_variables))
+
+
+		#target network
+		(self.feed('target_input')
+		.conv(8, 32, 4, 4, name = 'conv_target_1', appendList = self.__target_variables).max_pooling(name = 'max_pool_target_1')
+		.conv(4, 64, 2, 2, name = 'conv_target_2', appendList = self.__target_variables).max_pooling(name = 'max_pool_target_2')
+		.conv(3, 64, 1, 1, name = 'conv_target_3', appendList = self.__target_variables).max_pooling(name = 'max_pool_target_3')
+		.fc(512, name = 'fc_target_1', appendList = self.__target_variables)
+		.fc(self.__num_action, name = 'QValues_target', relu = False, appendList = self.__target_variables))
+
+		
+		self.__saver = tf.train.Saver()
+		ckpt = tf.train.get_checkpoint_state('model')
+		if ckpt and ckpt.model_checkpoint_path:
+				self.__saver.restore(self.__session, ckpt.model_checkpoint_path)
+				print 'Load trained model %s' %ckpt.model_checkpoint_path
+		else:
+				print 'No pretrained model found' 
+
+		#loss function
+		self.layers['action_input'] = tf.placeholder(tf.float32, shape = [None, num_action], name = 'action_input')
+		qvalues = tf.reduce_sum(tf.multiply(self.layers['QValues'], self.layers['action_input'] ),  reduction_indices = 1)
+		self.layers['y'] = tf.placeholder(tf.float32, shape = [None], name = 'target_values')
+		self.layers['loss_function'] = tf.reduce_mean(tf.square(self.layers['y'] - qvalues))
+		self.__optimizer = tf.train.AdamOptimizer(LEARNING_RATE).minimize(self.layers['loss_function'])
+
+		self.__session.run(tf.global_variables_initializer())
+
+	def copy_network(self):
+		for i in range(len(self.__train_variables)):
+			self.__session.run(self.__target_variables[i].assign(self.__train_variables[i]))
+
+	def preprocessing(self, img):
+		img = cv2.resize(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), IMAGE_SIZE)
+		ret, img = cv2.threshold(img, 1, 255, cv2.THRESH_BINARY)
+		img = np.reshape(img, [IMAGE_SIZE[0], IMAGE_SIZE[1], 1])
+		return img
+
+	def get_action(self):
+		action = np.zeros(self.__num_action)
+
+		if self.__time_step % FRAME_PER_ACTION == 0:
+			if random.random() < self.__ebsilon:
+				index = random.randint(0, self.__num_action - 1)
+			else:
+				qvalue = self.layers['QValues'].eval(feed_dict = {self.layers['train_input']:[self.__current_state]})[0]
+				index = np.argmax(qvalue)
+
+			action[index] = 1
+
+		elif HOLD_ACTION:
+			action =  self.__last_action
+		else:
+			action[0] = 1
+		
+		return action
+
+	def set_initial_state(self, observation):
+		observation = np.reshape(self.preprocessing(observation), IMAGE_SIZE)
+		self.__current_state = np.stack((observation, observation, observation, observation), axis = 2)
+
+	def train_model(self, epoch, ckpt_path):
+		pass
+
+	def iterate(self, observation, action, reward, terminate):
+		observation = self.preprocessing(observation)
+		newState= np.append(self.__current_state[:, :, 1:], observation, axis = 2)
+
+		#make experience
+		experience = (self.__current_state, action, newState, reward, terminate)
+		self.__replay_memory.append(experience)
+
+		self.__curren_tstate = newState
+
+		if self.__time_step > BATCH_SIZE:
+			self.train_model()
+
+		#reduce the explore rate
+		self.__time_step += 1
+		if self.__ebsilon >= 0 and self.__time_step <= EXPLORE_STEPS:
+			self.__ebsilon -= EBSILON / EXPLORE_STEPS
+
+		#remove the old experience if the replay memory is too large
+		if len(self.__replay_memory) > REPLAY_MEMORY_SIZE:
+			self.__replay_memory.popleft()
+
+		#save the model
+		if self.__time_step % SAVE_STEP == 0:
+			self.__saver.save(self.__session, CKPT_PATH, global_step = self.__time_step)
+
+		if self.__time_step % UPDATE_STEP == 0:
+			print 'Time step: %s, Explore rate: %s' %(self.__time_step, self.__ebsilon) 
+			self.copy_network()
+
+		
+	def train_model(self):
+		mini_batch = random.sample(self.__replay_memory, BATCH_SIZE)
+		state = [item[0] for item in mini_batch]
+		action = [item[1] for item in mini_batch]
+		newState = [item[2] for item in mini_batch]
+		reward = [item[3] for item in mini_batch]
+		terminate = [item[4] for item in mini_batch]
+
+		new_q = self.layers['QValues_target'].eval(feed_dict = {self.layers['target_input']:newState})
+		y = []
+		for i, q_value in enumerate(new_q):
+			if terminate[i] == True:
+				y.append(reward[i])
+			else:
+				y.append(reward[i] + GAMMA * np.max(q_value))
+		self.__optimizer.run(feed_dict = {self.layers['train_input']:state, self.layers['y']:y, self.layers['action_input']:action})
+
+if __name__ == '__main__':
+	num_action = 2
+	dqn = DQN(num_action)
+	flappyBird = game.GameState()
+
+	action0 = np.array([1,0])  # do nothing
+	observation0, reward0, terminal = flappyBird.frame_step(action0)
+	dqn.set_initial_state(observation0)
+
+	while True:
+		action = dqn.get_action()
+		nextObservation,reward,terminal = flappyBird.frame_step(action)
+		dqn.iterate(nextObservation, action, reward, terminal)
+
---
+++ network.py
@@ -0,0 +1,157 @@
+import tensorflow as tf
+import numpy as np
+import os
+import random
+import cv2
+
+def decorated_layer(layer):
+	def wrapper(self, *args,  **kwargs):
+		name = kwargs.setdefault('name', self.get_unique_name(layer.__name__))
+		
+		if len(self.inputs) == 1:
+			ipt = self.inputs[0]
+		else:
+			ipt = self.inputs
+
+		output = layer(self, ipt, *args, **kwargs)
+		self.layers[name] = output
+
+		return self.feed(output)
+
+	return wrapper
+
+class Network(object):
+	def __init__(self, dataset = None, trainable = True): #data_shape = (height, width)
+
+		self.__trainable = trainable
+		if self.__trainable == True:
+			assert not dataset == None
+			self.__dataset = dataset
+
+		self.__data_shape = dataset.shape
+		self.__num_cls = dataset.num_cls
+		self.__input_layer = tf.placeholder(tf.float32, shape = [None, self.__data_shape[0], self.__data_shape[1], self.__data_shape[2]], name = 'input')
+		self.__image_info = tf.placeholder(tf.float32, shape = [None, self.__num_cls], name = "label")
+		self.layers = {'input' : self.__input_layer, 'label' : self.__image_info}
+		self.inputs = []
+		
+		self.setup()
+
+	def feed(self,*args):
+		assert len(args) != 0
+		self.inputs = []
+
+		for ipt in args:
+			if isinstance(ipt, str):
+				try:
+					ipt = self.layers[ipt]
+					print(ipt)
+				except KeyError:
+					print('Existing layers:',self.layers.keys())
+					raise KeyError('Unknown layers %s' %ipt)
+			else:
+				print(ipt)
+			self.inputs.append(ipt)
+			
+		return self
+
+	def weight_variable(self, shape):
+		init = tf.truncated_normal(shape, stddev = 0.1)
+		return tf.Variable(init)
+
+	def bias_variable(self, shape):
+		init = tf.constant(0.1, shape = shape)
+		return tf.Variable(init)
+
+	def __append(self, appendList, variables):
+		if not appendList is None:
+			assert isinstance(appendList, list)
+			assert isinstance(variables, list)
+			assert len(variables) > 0
+			for item in variables:
+				appendList.append(item)
+
+
+	@decorated_layer
+	def conv(self,input_data, k_w, k_d, s_w, s_h, name, relu = True, padding = 'SAME', appendList = None):
+		
+		depth = input_data.get_shape().as_list()[-1]
+		#print('input data depth of ' + name + ':', depth)
+		#kernel
+		kernel = self.weight_variable([k_w, k_w, depth, k_d])
+
+		conv2d = tf.nn.conv2d(input_data, kernel, strides = [1, s_h, s_w, 1], padding = padding)
+
+		bias = self.bias_variable([k_d])
+
+		self.__append(appendList, [kernel, bias])
+
+		if relu:
+			return tf.nn.relu(tf.nn.bias_add(conv2d, bias), name = name)
+		else:
+			return tf.nn.bias_add(conv2d, bias, name = name)
+
+	@decorated_layer
+	def max_pooling(self, input_data, name, padding = 'SAME'):
+		return tf.nn.max_pool(input_data, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1],padding = padding, name = name)
+
+
+	@decorated_layer
+	def fc(self, input_data, output_dim, name, relu = True, appendList = None):
+		assert not isinstance(input_data, list)
+
+		shape = input_data.get_shape().as_list()
+		
+		size = 1
+
+		for i in shape[1:]:
+			size *= i
+
+		if len(shape) == 4:
+			input_data = tf.reshape(input_data, [-1, size])
+			#print(input_data.shape)
+		
+		w = self.weight_variable([size, output_dim])
+		b = self.bias_variable([output_dim])
+
+		self.__append(appendList, [w, b])
+
+		if relu:
+			op = tf.nn.relu_layer
+		else:
+			op = tf.nn.xw_plus_b
+
+		return op(input_data, w, b, name = name)
+
+
+	@decorated_layer
+	def soft_max(self, input_data, name, loss = True):
+		#print(input_data.get_shape().as_list())
+		if loss:
+			labels = self.layers['label']
+			return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = labels, logits = input_data), name = name)
+		else:
+			return tf.nn.softmax(input_data, name = name)
+
+	@decorated_layer
+	def drop_out(self, input_data, name, keep_prob_name):
+		kp = tf.placeholder(tf.float32, name = keep_prob_name)
+		self.layers[keep_prob_name] = kp
+		return tf.nn.dropout(input_data, keep_prob = kp, name = name)
+		
+	def setup(self):
+		raise NotImplementedError('Function setup(self) must be implemented!')
+
+			
+	def train_model(self, epoch, ckpt_path):
+		raise NotImplementedError('Function train_model(self, epoch, ckpt_path) must be implemented!')
+
+			
+	def im_detect(self, img):
+		raise NotImplementedError('Function im_detect(self, img) must be implemented!')
+
+	def get_unique_name(self, layer_name):
+		count = len([name for name, _ in self.layers.items() if name.startswith(layer_name)])
+		new_name = layer_name + '_' + str(count + 1)
+		return new_name
+
